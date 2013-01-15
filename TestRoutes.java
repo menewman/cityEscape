babc52dbe64e6de5749466a39544b82bcabc65fa
Allan <ajabri@gmail.com>
2013-01-15 05:09:31 -0500

    see above

--- TestRoutes.java
+++ TestRoutes.java
@@ -1,7 +1,11 @@
 public class TestRoutes {
 
     public static void main(String[] args) {
-        Routes rts = new Routes("Routes_Test_Files/unit_length_roads.txt", 10);
+        Routes rts = new Routes("Routes_Test_Files/unit_length_roads.txt", Integer.parseInt(args[0]));
+        for (int i = 0; i < 10; i++) {
         rts.draw();
+        rts.nextState();
+            StdDraw.show(100000);
+        }
     }
 }
